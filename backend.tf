f722ffe406141a115201b4501738de16b7f14f83
madhavareddy97056@gmail.com <madhavareddy97056@gmail.com>
2025-02-02 09:30:51 +0530

    tf files backend

--- backend.tf
+++ backend.tf
@@ -5,9 +5,5 @@ terraform {
     container_name        = "remotestate"
     key                   = "terraform.tfstate"
     
-    # use_azuread_auth      = true
-    # client_id             = "74cd3749-2d70-4ca8-b51d-f0da39520c21"  # Your Service Principal Client ID
-    # client_secret         = "iaO8Q~xfHC7PLsMtgPUg1lhWRUJ-d1wgbmIqEamT"      # Your Service Principal Secret
-    # tenant_id             = "03273300-90fd-446d-a8f8-e63a3ab71ec0"
   }
 }
